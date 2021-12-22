f5d9bb3e550472acfff7589d26ce974de679441d
Oleksandr Balitskiy <popingalov@gmail.com>
2021-12-22 23:29:14 +0300

    3

--- src/components/ImageGallery/Button/Button.js
+++ src/components/ImageGallery/Button/Button.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import s from './Button.module.css';
 export default function Button({ morePage, total }) {
   return (
@@ -7,3 +8,8 @@ export default function Button({ morePage, total }) {
     </button>
   );
 }
+
+Button.propTypes = {
+  morePage: PropTypes.func.isRequired,
+  total: PropTypes.number.isRequired,
+};
--- src/components/ImageGallery/status/Rejected.js
+++ src/components/ImageGallery/status/Rejected.js
@@ -1,3 +1,10 @@
+import PropTypes from 'prop-types';
 export default function Rejected({ error }) {
   return <h1>{error.message}</h1>;
 }
+
+Rejected.propTypes = {
+  error: PropTypes.shape({
+    message: PropTypes.string,
+  }),
+};
